534c36cde23e0fd4a829447e70786d798bbc9059
TheDreiDuck <agaman610@gmail.com>
2022-09-19 10:19:42 +0200

    exo1.2 terminé

---
+++ module1/exo1.2/index.js
@@ -0,0 +1,5 @@
+const dateTimeNow = new Date();
+const date = dateTimeNow.toLocaleDateString(); // 17/08/2020
+const hour = dateTimeNow.toLocaleTimeString();  
+
+alert(date + ' ' + hour + " : This is the best moment to have a look at this website !");
